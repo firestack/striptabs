c93fb2b4d50d4c33d878a1de570f8cba338f2ca6
Kayla Firestack <dev@kaylafire.me>
2022-05-28 17:42:25 -0400

    feat(flake/stripTabs): make stripTabs take lib as a function input, move function into let block

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
 	description = "Strip tabs, for when nix fails you";
 
-	outputs = { self, nixpkgs }: let inherit (nixpkgs) lib; in {
-		# Packaged from https://github.com/NixOS/nix/issues/3759#issuecomment-653033810
-
-		lib.stripTabs = text: let
+	outputs = { self, nixpkgs }: let 
+		stripTabsFn = lib: text: let
 			# Whether all lines start with a tab (or is empty)
 			shouldStripTab = lines: builtins.all (line: (line == "") || (lib.strings.hasPrefix "	" line)) lines;
 			# Strip a leading tab from all lines
@@ -14,6 +12,10 @@
 		in
 			# Split into lines. Strip leading tabs. Concat back to string.
 			builtins.concatStringsSep "\n" (stripTabs (lib.strings.splitString "\n" text));
+	in {
+		# Packaged from https://github.com/NixOS/nix/issues/3759#issuecomment-653033810
+
+		lib.stripTabs = stripTabsFn nixpkgs.lib;
 
 
 	};
