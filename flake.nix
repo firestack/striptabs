ffc812228909e51532e6032785359be19a072940
Kayla Firestack <dev@kaylafire.me>
2022-05-29 19:27:58 -0400

    doc(flake): clarify function

--- flake.nix
+++ flake.nix
@@ -8,16 +8,19 @@
 		# Packaged from https://github.com/NixOS/nix/issues/3759#issuecomment-653033810
 		stripTabsFn = lib: text: let
 			# Whether all lines start with a tab (or is empty)
-			shouldStripTab = lines: builtins.all (line: (line == "") || (lib.strings.hasPrefix "	" line)) lines;
+			shouldStripTab = lines: builtins.all (line: (line == "") || (lib.strings.hasPrefix "\t" line)) lines;
 			# Strip a leading tab from all lines
-			stripTab = lines: builtins.map (line: lib.strings.removePrefix "	" line) lines;
-			# Strip tabs recursively until there are none
-			stripTabs = lines: if (shouldStripTab lines) then (stripTabs (stripTab lines)) else lines;
+			stripTab = lines: builtins.map (line: lib.strings.removePrefix "\t" line) lines;
+			# Strip tabs from lines recursively until there are none
+			stripTabs = lines:
+				if (builtins.all (line: line == "") lines) then lines
+				else if (shouldStripTab lines) then (stripTabs (stripTab lines))
+				else lines;
 		in
 			# Split into lines. Strip leading tabs. Concat back to string.
 			builtins.concatStringsSep "\n" (stripTabs (lib.strings.splitString "\n" text));
-	in {
 
+	in {
 		lib.stripTabs = stripTabsFn nixpkgs.lib;
 
 		/*
