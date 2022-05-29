6a9f11c3f2c7bd128b2a045c60992556ede2e941
Kayla Firestack <dev@kaylafire.me>
2022-05-28 21:35:27 -0400

    reword! add tests

--- flake.nix
+++ flake.nix
@@ -16,10 +16,42 @@
 		in
 			# Split into lines. Strip leading tabs. Concat back to string.
 			builtins.concatStringsSep "\n" (stripTabs (lib.strings.splitString "\n" text));
+
 	in {
 		# Packaged from https://github.com/NixOS/nix/issues/3759#issuecomment-653033810
 
-		lib.stripTabs = stripTabsFn nixpkgs.lib;
+		lib.stripTabs = let 
+			stripTabs = stripTabsFn nixpkgs.lib;
+
+			evaluatedTests = builtins.mapAttrs
+				(_test_name: test_case: test_case // rec {
+					input = stripTabs test_case.input;
+					comparison = [ test_case.expected input ];
+				})
+				self.nixTests.text.cases;
+
+			assertions = fn:
+				let
+					testFn = test_name: test_data:
+						lib.asserts.assertMsg 
+							(with test_data; expected == input)
+							stripTabs ''
+								`input` differs from `expected`
+								::  Expected ::
+								${test_data.expected}
+								
+								::  Recieved ::
+								${test_data.input}
+							'';
+				in
+					assert builtins.all
+						({test_name, test_data}: testFn test_name test_data)
+						(lib.attrsets.mapAttrsToList
+							(test_name: test_data: { inherit test_name test_data; })
+							evaluatedTests);
+					fn;
+
+		in assertions stripTabs;
 
 		/*
 		# QOL Utilitiy Attributes
@@ -40,9 +72,79 @@
 		or
 			```pkgs.stripTabs ''<multiline-tab-indented-string>''```
 		*/
+		overlays.default = self.overlays.stripTabs;
+
 		overlays.stripTabs = final: prev: {
 			stripTabs = stripTabsFn final.lib;
 		};
-		overlay = self.overlays.stripTabs;
-	};
+
+		nixTests.text.fn.assertMsg = { test_name, test_data }:
+			lib.asserts.assertMsg
+				(nixTests.text.fn.predicate test_data)
+				(nixTests.text.fn.errorMessage test_name test_data);
+		nixTests.text.fn.predicate = case: (case.expected == case.input);
+		nixTests.text.fn.errorMessage = stripTabs: test_name: { expected, input }: stripTabs ''
+			:: Error In Test :: ${test_name}
+			`input` differs from `expected`
+
+			::  Expected ::
+			${expected}
+			
+			::  Recieved ::
+			${input}
+		'';
+
+		nixTests.text.cases = {
+			# empty = {
+			# 	input = '''';
+			# 	expected = "";
+			# };
+
+			trivial = {
+				input = ''indent0'';
+				expected = "indent0";
+			};
+
+			trivial_newline = {
+				input = ''
+					indent0
+				'';
+				expected = "indent0\n";
+			};
+			
+			trivial_newline2 = {
+				input = ''
+					indent0
+					indent0
+					indent0
+				'';
+				expected = "indent0\nindent0\nindent0\n";
+			};
+			
+			trivial_indent = {
+				input = ''
+					indent0
+						indent1
+					indent0
+				'';
+				expected = "indent0\n\tindent1\nindent0\n";
+			};
+
+			multiple_indent_levels = {
+				input = ''
+					indent0
+							indent2
+						indent1
+					indent0
+				'';
+				expected = "indent0\n\t\tindent2\n\tindent1\nindent0\n";
+			};
+		};
+	}
+	// utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+		inherit (utils.lib.check-utils system) isEqual; 
+		isDataTestEqual = fn: { input, expected }: isEqual input (fn expected);
+	in {
+		# checks = builtins.mapAttrs (_test_name: isDataTestEqual self.stripTabs) self.nixTests.text.cases;
+	});
 }
